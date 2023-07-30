e91216b81982c9e0901698197eae6c16001f77f1
AlexMuzzy <alexmuzzy33@gmail.com>
2023-07-30 22:27:20 +0100

    intro responsive styling

--- src/Intro.tsx
+++ src/Intro.tsx
@@ -1,9 +1,9 @@
 import profileImage from "./assets/1654880800553.jpg";
 
 const Intro = () => (
-  <div className="m-auto flex w-1/4 flex-col rounded">
+  <div className="m-auto flex w-1/2 flex-col rounded md:w-1/3 lg:w-1/4">
     <img src={profileImage} className="rounded-full" />
-    <p className="mt-4 text-2xl">Software Engineer</p>
+    <p className="mt-4 text-center text-2xl">Software Engineer</p>
   </div>
 );
 
