bc0a49e698dbbb3c6cbfd9c57e23fa985ae7f762
AlexMuzzy <alexmuzzy33@gmail.com>
2023-07-31 00:05:42 +0100

    remove annoying ah key error

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -13,28 +13,35 @@ const Contact = () => {
 
   const socialMediaIcons = [
     {
+      name: "Facebook",
       icon: FaFacebookSquare,
       link: "https://www.facebook.com/AlexMuzey/",
     },
     {
+      name: "Github",
       icon: FaGithubSquare,
       link: "https://github.com/AlexMuzzy",
     },
     {
+      name: "Instagram",
       icon: FaInstagramSquare,
       link: "https://www.instagram.com/alex.muzzy/",
     },
     {
+      name: "LinkedIn",
       icon: FaLinkedin,
       link: "https://www.linkedin.com/in/alex-musgrove-242a0b16a/",
     },
   ];
 
   return (
-    <div className="flex w-screen flex-row justify-center gap-8">
+    <div className="flex flex-row justify-center gap-8">
       {socialMediaIcons.map((socialMediaIcon) => {
         return (
-          <a href={socialMediaIcon.link}>
+          <a
+            href={socialMediaIcon.link}
+            key={`media-icon-${socialMediaIcon.name}`}
+          >
             <IconContext.Provider
               value={{
                 className: "contactIcon",
