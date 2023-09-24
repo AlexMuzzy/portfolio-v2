0ea0ab84cfe645836ac0f0d9e3dadceab98f50e7
AlexMuzzy <alexmuzzy33@gmail.com>
2023-09-24 23:58:41 +0100

    revert dropdown colour changes

--- src/Sections/BackgroundMenu.tsx
+++ src/Sections/BackgroundMenu.tsx
@@ -88,7 +88,7 @@ const BackgroundMenu = ({
             className="z-10 w-44 divide-y divide-gray-100 rounded-lg bg-white shadow dark:divide-gray-600 dark:bg-gray-700"
           >
             <ul
-              className="py-2 text-sm"
+              className="py-2 text-sm text-gray-700 dark:text-gray-200"
               aria-labelledby="dropdownDividerButton"
             >
               {colours.map((colour) => {
